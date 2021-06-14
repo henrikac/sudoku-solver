d95b034ab4a436c2409f1f6408c9fa885490576f
Henrik Christensen <sensen1695@hotmail.com>
2021-06-14 11:22:10 +0200

    implemented solve method

--- spec/sudoku-solver_spec.cr
+++ spec/sudoku-solver_spec.cr
@@ -1,9 +1,40 @@
 require "./spec_helper"
 
 describe Sudoku::Solver do
-  # TODO: Write tests
+  describe "solve" do
+    it "should solve the sudoku puzzle and return the result" do
+      input = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+      ]
+      board = Sudoku::Board.new(input)
 
-  it "works" do
-    false.should eq(true)
+      expected = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+      ]
+      solved = Sudoku::Solver.solve(board)
+
+      solved.should be_true
+      board.to_a.each_with_index do |row, i|
+        row.each_with_index do |field, j|
+          field.should eq expected[i][j]
+        end
+      end
+    end
   end
 end
--- src/sudoku-solver.cr
+++ src/sudoku-solver.cr
@@ -1,5 +1,51 @@
+require "./sudoku-solver/**"
+
 # TODO: Write documentation for `Sudoku::Solver`
 module Sudoku::Solver
 
-  # TODO: Put your code here
+  def self.solve(board : Sudoku::Board) : Bool
+    coords_empty_field = find_empty_field(board)
+    return true if coords_empty_field.nil?
+
+    row, col = coords_empty_field
+    guess = 1
+
+    loop do
+      if valid_try(board, coords_empty_field, guess)
+        board[row][col] = guess
+
+        return true if solve(board)
+
+        board[row][col] = 0
+      end
+
+      guess += 1
+      break if guess == 10
+    end
+
+    return false
+  end
+
+  private def self.valid_try(board : Sudoku::Board, coords : Tuple(Int32, Int32), num : Int32) : Bool
+    row = board[coords[0]]
+    col = board.get_column(coords[1])
+
+    return false if row.includes?(num)
+    return false if col.includes?(num)
+
+    region = board.get_region(coords)
+
+    return false if region.includes?(num)
+
+    return true
+  end
+
+  private def self.find_empty_field(board : Sudoku::Board) : Nil | Tuple(Int32, Int32)
+    board.to_a.each_with_index do |row, i|
+      row.each_with_index do |field, j|
+        return {i, j} if board[i][j] == Sudoku::Board::EMPTY_FIELD
+      end
+    end
+    return nil
+  end
 end
